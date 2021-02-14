382a45d897eb35401c92a362ed76f5f71689c1b5
David Bernheisel <david@bernheisel.com>
2021-02-14 01:17:16 -0500

    Add GitHub CI

--- config/test.exs
+++ config/test.exs
@@ -5,10 +5,17 @@ use Mix.Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :petal, Petal.Repo,
-  database: "petal_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+
+if System.get_env("CI") do
+  config :petal, Petal.Repo,
+    url: "postgres://postgres:postgres@postgres:5432/utility_test#{System.get_env("MIX_TEST_PARTITION")}",
+    pool: Ecto.Adapters.SQL.Sandbox
+else
+  config :petal, Petal.Repo,
+    database: "petal_test#{System.get_env("MIX_TEST_PARTITION")}",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
